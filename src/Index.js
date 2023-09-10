ea79fe0cbe48b4ad29bc022c95ac7c7c3ebf47f7
KarolCifuentes <karolsofi16@gmail.com>
2023-09-10 17:00:19 -0500

    primer commit ProductManager

---
+++ src/Index.js
@@ -0,0 +1,17 @@
+/*var foo = {
+  get bar() {
+    return this._bar;
+  },
+  set bar(value) {
+    this._bar = value;
+  },
+};*/
+
+Class ProductManager {
+    constructor(getProducts){
+
+    this.getProducts = getProducts
+
+ }
+
+}
