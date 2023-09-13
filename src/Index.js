ba26a639c659754d8338cc9fa0446c6352bb60cf
CrizarDevelop <crizardevelop@gmail.com>
2023-09-12 21:45:39 -0500

    Tercer commit ProductManager

--- src/Index.js
+++ src/Index.js
@@ -26,37 +26,57 @@ class ProductManager {
 
     static id = 0
 
-    addProduct(title, description, price, thumbnail, code, stock){
+    addProducts(title, description, price, thumbnail, code, stock){
 
-        ProductManager.id++
-        this.products.push({title, description, price, thumbnail, code, stock, id:ProductManager.id});
+        for(let i = 0; i < this.products.length; i ++ ){
+
+            if(this.products[i].code === code){
+            console.log(`El codigo ${code} esta repetido`);
+            break;
+            }
+        }
+
+        const newProduct ={
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+        }
+            /*console.log(Object.values(newProduct))*/
+
+            if(!Object.values(newProduct).includes(undefined)){
+                ProductManager.id++;
+                this.products.push({
+                    ...newProduct,
+                    id:ProductManager.id,
+                });
+            }else{
+                console.log("Todos los campos son requeridos")
+            }
     }
 
     getProducts(){
         return this.products;
     }
 
+    existe(id){
+        return this.products.find((product) => product.id === id)
+
+    }
     getProductsById(id){
-        if(!this.products.find((product) => products.id === id)){
-            console.log("Not Found")
-
-        }else{
-            console.log("Existe")
-        }
-
+        !this.existe(id) ? console.log("Not Found") : console.log(this.existe(id));
     }
 }
     const products = new ProductManager
-    /*Primera llamada = arreglo vacio*/
+
     console.log(products.getProducts());
 
-    /*Agrgar productos*/
-    console.log(products.addProduct('product1', 'description1', '1000', 'thumbnail1', '1234', '10'))
-    console.log(products.addProduct('product2', 'description2', '2000', 'thumbnail1', '5678', '20'))
+    console.log(products.addProducts('product1', 'description1', '1000', 'thumbnail1', '1234', '10'))
+    console.log(products.addProducts('product2', 'description2', '2000', /* 'thumbnail1',*/'4567', '20'))
 
-
-    /*Segunda llamada = arreglo con productos*/
     console.log(products.getProducts());
+    console.log(products.addProducts('Manzana', 'Fruta', '1000', 'thumbnail1', '1234', '1'))
 
-
-    products.getProductsById(2)
+    products.getProductsById(3)
